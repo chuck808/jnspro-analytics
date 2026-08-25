f4ac8d0cc2a16ad38dd9f0658f51c93e3cda0258
chuck808 <chuck808@users.noreply.github.com>
2026-08-25 13:25:14 +0100

    chore: remove obsolete runtime docs manager loader

--- src/routes/(protected)/admin/docs-manager/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { requireAdminFromProfile } from '$lib/server/adminAuth';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ parent }) => {
-	const { profile } = await parent();
-	requireAdminFromProfile(profile);
-	return {};
-};
